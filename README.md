a146445f7d2c1d192fbfb398ffe062fc11a15139
MrSnor <parthap.das36@gmail.com>
2021-12-27 09:24:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# whack a mole
+
+Simple javascript game that I made using https://github.com/kubowania/whac-a-mole as the base.
---
+++ scripts/app.js
@@ -0,0 +1,57 @@
+alert("smash the mole to earn points")
+
+const squares = document.querySelectorAll(".square")
+const mole = document.querySelector(".mole")
+const timeLeft = document.querySelector("#time-left")
+const score = document.querySelector("#score")
+
+let result = 0
+let currentTime = 60
+let hitPosition = null
+let timerId = null
+let countDownTimerId = null
+
+// to choose a random square
+function randomSquare() {
+    // removing mole class and event listener each time new square is chosen
+    squares.forEach(square => {
+        square.classList.remove("mole")
+        square.removeEventListener("mousedown", scorer)
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add("mole")
+
+    // assigning the id of randomSquare to hitPosition
+    hitPosition = randomSquare.id
+
+    randomSquare.addEventListener("mousedown", scorer)
+}
+
+function scorer(square) {
+    if (square.id = hitPosition) {
+        result++
+        score.textContent = result
+        hitPosition = null
+    }
+}
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 500)
+}
+
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if (currentTime === 0) {
+        hitPosition = null
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert("Your time is up! Final score is : " + result)
+    }
+}
+
+moveMole()
+countDownTimerId = setInterval(countDown, 1000)
